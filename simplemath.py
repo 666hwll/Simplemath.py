2559158c629490700c04a7dc05d7bd4efc037263
MiraiX <117575492+666hwll@users.noreply.github.com>
2024-09-08 09:43:49 +0200

    added newer version of simplemath

--- simplemath.py
+++ simplemath.py
@@ -2,29 +2,46 @@ import random
 
 rng = random.SystemRandom()
 
+
 def getmode() -> int:
-    mode = (1, 10, 100)
+    #mode = (1, 10, 100)
+    new_modes = (1, 2, 3)
     print("How hard should the equations be?\n [0/1/2]")
     inpt = int(input(">> "))
     if inpt >= 0 and not inpt > 2:
-        return mode[inpt]
+        return new_modes[inpt]
     else:
         print("WTF are you retarded?")
         exit()
         return 0
 
+def gen_num(para: int, frn) -> int:
+    enum = 0
+    substr = ""
+    while enum < para:
+        s = str(rng.randint(frn,11)) 
+        substr += s
+        enum += 1
+    return int(substr)
+
+def eva_lenght_of_eq():
+    pass
 
 def main():
     ptr = getmode()
     inpt = str
-    while inpt != 'X':  
-        firDig = rng.randint(0,11) * ptr
+    while inpt != 'X':
+        ####
+        #firDig = rng.randint(0,11) * ptr
+        firDig = gen_num(ptr, 0)
         opRAN = rng.choice(["+", "-", "*", "/"])
         if opRAN != '/':
             frn = 0
         else:
             frn = 1
-        secDig = rng.randint(frn,11) * ptr
+        #secDig = rng.randint(frn,11) * ptr
+        secDig = gen_num(ptr, frn)
+        ####
         eq = str(firDig) + opRAN + str(secDig)
         print(f"Type in the outcome of {eq}")
         solution = eval(eq)
